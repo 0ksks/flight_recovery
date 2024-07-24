3cd71e47e3dcf1a6729409b36838cb8104e1dda0
yxluo <1625436933@qq.com>
2024-07-24 16:11:30 +0800

    update gitignore

--- RL/Env.py
+++ RL/Env.py
@@ -7,6 +7,8 @@ import numpy as np
 ObsType = TypeVar("ObsType", bound=torch.Tensor)
 ActType = TypeVar("ActType", bound=torch.Tensor)
 RenderFrame = TypeVar("RenderFrame")
+NO_ACTION: ActType = torch.tensor(0)
+NO_OBS: ObsType = torch.tensor(0)
 
 
 class DAGEnv(gym.Env):
@@ -17,26 +19,28 @@ class DAGEnv(gym.Env):
         self.topology = topology
         self.visited = torch.zeros(topology.shape[1], dtype=torch.bool)
 
-    def step(self, action: ActType):
-        next_state_ = action
-        self.update_visited(action)
+    def step(self, action_: ActType):
+        next_state_ = action_
+        self.update_visited(action_)
         reward_ = 1
-        terminal_ = self.query_unvisited(next_state_).numel() == 0
+        terminal_ = torch.all(self.visited)
         truncated_ = False
         info_ = {}
         return next_state_, reward_, terminal_, truncated_, info_
 
-    def query_unvisited(self, vertex) -> torch.Tensor:
-        return self.topology[:, (self.topology[0, :] == vertex) & (~self.visited)][1, :]
+    def query_unvisited(self, vertex_) -> torch.Tensor:
+        return self.topology[:, (self.topology[0, :] == vertex_) & (~self.visited)][
+            1, :
+        ]
 
-    def update_visited(self, vertex) -> None:
-        self.visited[self.topology[1, :] == vertex] = True
+    def update_visited(self, vertex_) -> None:
+        self.visited[self.topology[1, :] == vertex_] = True
 
     def reset(
         self,
         *,
-        seed: Union[int, None] = None,
-        options: Union[dict[str, Any], None] = None,
+        seed_: Union[int, None] = None,
+        options_: Union[dict[str, Any], None] = None,
     ) -> tuple[ObsType, dict[str, Any]]:
         self.visited = self.topology
         return self.visited, {}
@@ -45,21 +49,27 @@ class DAGEnv(gym.Env):
 
 
 class DAGAgentBase:
-    def __init__(self, start, end) -> None:
-        self.start = start
-        self.end = end
+    def __init__(self, start_, end_) -> None:
+        self.start = start_
+        self.end = end_
 
     def choose_action(self, *args, **kwargs) -> ObsType: ...
 
 
 class DAGAgentRollout(DAGAgentBase):
-    def __init__(self, start, end) -> None:
-        super().__init__(start, end)
+    def __init__(self, start_, end_) -> None:
+        super().__init__(start_, end_)
 
-    def choose_action(self, action_space) -> tuple[ActType, bool]:
-        action_ = np.random.choice(action_space)
-        terminal_ = action_ == self.end
-        return action_, terminal_
+    def choose_action(self, state_, env: DAGEnv) -> tuple[ActType, bool]:
+        action_space_ = env.query_unvisited(state_)
+        if action_space_.numel() == 0:
+            action_ = NO_ACTION
+            terminal_ = True
+            return action_, terminal_
+        else:
+            action_ = torch.tensor(np.random.choice(action_space_))
+            terminal_ = action_ == self.end
+            return action_, terminal_
 
 
 if __name__ == "__main__":
@@ -72,13 +82,10 @@ if __name__ == "__main__":
     dagAgentRollout = DAGAgentRollout(1, 9)
 
     state = 1
-    state_history = [
-        state,
-    ]
+    state_history = [state]
     done = False
     while not done:
-        cur_action_space = dagEnv.query_unvisited(state)
-        cur_action, agent_terminal = dagAgentRollout.choose_action(cur_action_space)
+        cur_action, agent_terminal = dagAgentRollout.choose_action(state, dagEnv)
         next_state, reward, env_terminal, env_truncated, _ = dagEnv.step(cur_action)
         done = agent_terminal or env_terminal or env_truncated
         state_history.append(next_state)
--- test.py
+++
@@ -1,4 +0,0 @@
-import torch
-
-a = torch.tensor([1, 2, 3, 4, 3, 4], dtype=torch.long)
-print()
