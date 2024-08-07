bc0ba4adb8b870f06cfe8413b78452bceed9976b
yxluo <1625436933@qq.com>
2024-08-07 13:46:51 +0800

    test rollout, remain mask

--- RL/Env.py
+++ RL/Env.py
@@ -1,3 +1,4 @@
+import random
 from typing import Union
 import numpy as np
 from RL import AdjList
@@ -25,7 +26,7 @@ class DAGmap:
         all_next = self.adj_list[state_idx]
         return all_next[~self.__visited[all_next]]
 
-    def actor(self, state: int, mask: list[bool]) -> tuple[list[int], list[float]]:
+    def actor(self, state: int, mask: list[bool]) -> list[float]:
         assert (
             self.actor_network is not None
         ), "actor network is not initialized, use `self.register_network(actor_network, critic_network)`"
@@ -58,45 +59,45 @@ class DAGmap:
 
 class Aeroplane:
     def __init__(self, start_state: int, end_state: int, reward=1, info=None):
-        self.__start_state = start_state
-        self.__end_state = end_state
-        self.__current_state = self.__start_state
+        self.start_state = start_state
+        self.end_state = end_state
+        self.current_state = self.start_state
         self.reward = reward
         self.info = info
-        self.__actor_network = None
-        self.__critic_network = None
+        self.actor_network = None
+        self.critic_network = None
 
     def register_network(self, actor_network=None, critic_network=None):
-        self.__actor_network = actor_network
-        self.__critic_network = critic_network
+        self.actor_network = actor_network
+        self.critic_network = critic_network
 
     def actor(
         self, aeroplane_state: int, dag_state: list[bool], mask: list[bool]
     ) -> list[float]:
         assert (
-            self.__actor_network is not None
+            self.actor_network is not None
         ), "actor network is not initialized, use `self.register_network(actor_network, critic_network)`"
-        return self.__actor_network(aeroplane_state, dag_state, mask)
+        return self.actor_network(aeroplane_state, dag_state, mask)
 
     def critic(self, aeroplane_state: int, dag_state: list[bool], action: int) -> float:
         assert (
-            self.__critic_network is not None
+            self.critic_network is not None
         ), "critic network is not initialized, use `self.register_network(actor_network, critic_network)`"
-        return self.__critic_network(aeroplane_state, dag_state, action)
+        return self.critic_network(aeroplane_state, dag_state, action)
 
     def step(
         self,
         aeroplane_action: int,
         dag_state: list[bool],
     ):
-        self.__current_state = aeroplane_action
+        self.current_state = aeroplane_action
         reward = self.reward
-        terminated = self.__current_state == self.__end_state
+        terminated = self.current_state == self.end_state
         return reward, terminated
 
     def get_state(self):
         """current state int"""
-        return self.__current_state
+        return self.current_state
 
 
 class MultiAeroplane:
@@ -117,6 +118,9 @@ class MultiAeroplane:
                 start_state_array, end_state_array, reward_array, info_array
             )
         ]
+        self.start_state = start_state_array
+        self.end_state = end_state_array
+        self.aeroplane_num = len(self.aeroplanes)
 
     def actor(
         self,
@@ -126,14 +130,17 @@ class MultiAeroplane:
     ) -> th.Tensor:
         """
         return the index of the best action in each row
-        :return: max_index: (1 * agent_num)
+        :param aeroplane_state_array: current state for each aeroplane (1 * n_aeroplanes)
+        :param dag_state: visited nodes (1 * n_nodes)
+        :param mask_array: mask for each aeroplane (n_aeroplanes * n_aeroplane_states)
+        :return: max_index: (1 * n_aeroplanes)
         """
         prob = []
         for aeroplane, aeroplane_state, dag_state, mask in zip(
             self.aeroplanes, aeroplane_state_array, dag_state, mask_array
         ):
             prob.append(aeroplane.actor(aeroplane_state, dag_state, mask))
-        prob = th.tensor(prob)
+        prob = th.tensor(np.array(prob))
         decisions = MultiAct.decide(prob)
         return decisions
 
@@ -148,7 +155,7 @@ class MultiAeroplane:
             self.aeroplanes, aeroplane_state_array, dag_state, action_array
         ):
             q_value.append(aeroplane.critic(aeroplane_state, dag_state, action))
-        q_value = th.tensor(q_value)
+        q_value = th.tensor(np.array(q_value))
         return q_value
 
     def step(
@@ -217,11 +224,27 @@ if __name__ == "__main__":
     # print(aeroplane_test.step(4, None))
     # print(aeroplane_test.get_state())
 
-    multi_aeroplane_test = MultiAeroplane([1, 2, 3, 4], [5, 6, 7, 8])
+    multi_aeroplane_test = MultiAeroplane(
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+    )
     # print(multi_aeroplane_test.step([2, 3, 4, 5], None))
     # print(multi_aeroplane_test.step([3, 4, 5, 6], None))
     # print(multi_aeroplane_test.step([5, 6, 7, 8], None))
-    multi_aeroplane_test.actor(
-        [1, 2, 3, 4], [True, True, False, False], [True, True, False, False]
+    mask_array_np = np.random.randint(0, 2, (4, 8))
+    mask_array_tensor = th.tensor(mask_array_np).to(dtype=th.bool)
+
+    def test_actor(state, dag, mask):
+        return np.random.randint(0, 2, (mask.shape[0],))
+
+    for aeroplane in multi_aeroplane_test.aeroplanes:
+        aeroplane.register_network(actor_network=test_actor)
+
+    actions = multi_aeroplane_test.actor(
+        [0, 1, 2, 3],
+        [True, True, True, True, False, False, False, False],
+        mask_array_tensor,
     )
+    output = multi_aeroplane_test.step(actions, None)
     print(multi_aeroplane_test.get_state())
+    print(output)
--- RL/Trainer.py
+++ RL/Trainer.py
@@ -2,6 +2,7 @@ from typing import Union
 from dataclasses import dataclass
 from Env import DAGmap, MultiAeroplane
 import torch as th
+import numpy as np
 
 
 @dataclass
@@ -35,7 +36,7 @@ class Rollout:
 
     def __init_start_node(self):
         start_node_array = [
-            aeroplane.start_node for aeroplane in self.multiAeroplane.aeroplanes
+            aeroplane.start_state for aeroplane in self.multiAeroplane.aeroplanes
         ]
         self.dag.visit(start_node_array)
 
@@ -43,12 +44,11 @@ class Rollout:
         self,
         episode_steps: int,
         dag_mask: list[bool],
-        aeroplanes_mask_array: Union[list[list[bool]], th.Tensor],
     ) -> tuple[list[Experience], list[Experience]]:
         """
         :param episode_steps:
         :param dag_mask:
-        :param aeroplanes_mask_array:
+        :param aeroplane_mask_array:
         :return: DAG episode, aeroplane episode
         """
         step = 0
@@ -57,22 +57,31 @@ class Rollout:
         # aeroplane_state_array = []
         # for aeroplane in self.multiAeroplane.aeroplanes:
         #     aeroplane_state_array.append(aeroplane.current_node)
-        aeroplane_state_array = list(
-            map(
-                lambda aeroplane: aeroplane.current_node, self.multiAeroplane.aeroplanes
-            )
-        )
-        aeroplane_state_array = th.tensor(aeroplane_state_array)
+
+        # aeroplane_state_array = list(
+        #     map(
+        #         lambda aeroplane: aeroplane.current_state,
+        #         self.multiAeroplane.aeroplanes,
+        #     )
+        # )
+
         dag_episode = []
         aeroplane_episode = []
         while step < episode_steps:
+            dag_state = self.dag.get_state()
+            aeroplane_state_array_tensor = self.multiAeroplane.get_state().flatten()
+            dag_state[aeroplane_state_array_tensor] = True
             # dag acts
-            dag_action, _ = self.dag.actor(state=dag_state, mask=dag_mask)
+            dag_action_prob = np.array(self.dag.actor(state=dag_state, mask=dag_mask))
+            dag_action = np.where(dag_action_prob == dag_action_prob.max(), 1, 0)
             # aeroplanes act
+            aeroplane_mask_array = [
+                dag_state,
+            ] * self.multiAeroplane.aeroplane_num
             aeroplane_action_array_tensor = self.multiAeroplane.actor(
-                aeroplane_state_array=aeroplane_state_array,
+                aeroplane_state_array=aeroplane_state_array_tensor,
                 dag_state=dag_state,
-                mask_array=aeroplanes_mask_array,
+                mask_array=aeroplane_mask_array,
             )
             # get dag info
             dag_reward, dag_terminated, dag_new_state = self.dag.step(dag_action)
@@ -89,8 +98,6 @@ class Rollout:
             dag_action_tensor = th.tensor(dag_action)
             dag_reward_tensor = th.tensor(dag_reward)
             dag_terminated_tensor = th.tensor(dag_terminated)
-
-            aeroplane_state_array_tensor = th.tensor(aeroplane_state_array)
             # store experience
             dag_episode.append(
                 Experience(
@@ -109,12 +116,74 @@ class Rollout:
                     aeroplane_terminated_array_tensor,
                 )
             )
-            # update dag and aeroplanes state
-            dag_state = self.dag.get_state()
-            aeroplane_state_array = self.multiAeroplane.get_state()
             # update step
             step += 1
             # termination check
             if dag_terminated_tensor.all():
                 break
         return dag_episode, aeroplane_episode
+
+
+if __name__ == "__main__":
+    aeroplane_num = 4
+    node_num = 16
+
+    def np_array_adj_list(adj_list: dict[int, list[int]]):
+        adj_list_array: dict[int, np.ndarray] = {}
+        for k, v in adj_list.items():
+            adj_list_array[k] = np.array(v).astype(int)
+        return adj_list_array
+
+    graph = {
+        0: [4, 5],
+        1: [6],
+        2: [7],
+        3: [8, 9],
+        4: [10],
+        5: [11],
+        6: [12],
+        7: [13],
+        8: [14],
+        9: [15],
+        10: [],
+        11: [],
+        12: [],
+        13: [],
+        14: [],
+        15: [],
+    }
+    dag = DAGmap(
+        np_array_adj_list(graph),
+    )
+    multi_aeroplane_test = MultiAeroplane(
+        [0, 1, 2, 3],
+        [12, 13, 14, 15],
+    )
+    dag_mask = np.zeros(node_num).astype(bool)
+    dag_mask[multi_aeroplane_test.start_state] = True
+    dag_mask = dag_mask.tolist()
+
+    def aeroplane_test_actor(state_, env_state_, mask_):
+        mask_ = np.array(mask_)
+        prob = np.random.rand(mask_.shape[0])
+        return prob
+
+    def dag_test_actor(state_, mask_):
+        return aeroplane_test_actor(state_, None, mask_)
+
+    for aeroplane in multi_aeroplane_test.aeroplanes:
+        aeroplane.register_network(actor_network=aeroplane_test_actor)
+
+    rollout_test = Rollout(dag, multi_aeroplane_test)
+    rollout_test.dag_network_register(actor_network=dag_test_actor, critic_network=None)
+    multi_aeroplane_actor = [aeroplane_test_actor for _ in range(aeroplane_num)]
+    multi_aeroplane_critic = [None for _ in range(aeroplane_num)]
+    rollout_test.multi_aeroplane_network_register(
+        multi_aeroplane_actor, multi_aeroplane_critic
+    )
+    dag_episode, aeroplane_episode = rollout_test.rollout_episode(
+        dag_mask=dag_mask,
+        episode_steps=8,
+    )
+    print(dag_episode)
+    print(aeroplane_episode)
